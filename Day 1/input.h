8bdc38702e6294722c22bc026e88a6a28f2b1f19
Adam Clay <adamclayeng@vocal.com>
2026-01-09 11:58:43 -0500

    Add project files.

---
+++ Day 1/input.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* ********************************************************************************************** *
+ *                                        Standard Library                                        *
+ * ********************************************************************************************** */
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::istream;
+using std::ostream;
+
+#include <vector>
+using std::vector;
+using std::begin;
+using std::end;
+
+#include <string>
+using std::string;
+
+/* ********************************************************************************************** *
+ *                                          Puzzle Input                                          *
+ * ********************************************************************************************** */
+
+
