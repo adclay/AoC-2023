2861f03784b69006e9964ef44a53b4c2de32abed
Adam Clay <adamclayeng@vocal.com>
2026-01-27 15:47:33 -0500

    Solved day 16.

---
+++ Day 16/part1.cpp
@@ -0,0 +1,95 @@
+#include "header.h"
+
+inline void move_forward(char dir, int &y, int &x) {
+	switch (dir) {
+	case '^':
+		--y;
+		break;
+	case '<':
+		--x;
+		break;
+	case 'v':
+		++y;
+		break;
+	case '>':
+		++x;
+		break;
+	default:
+		return;
+	}
+}
+
+int part1(std::istream &fin) {
+	vector<string> tiles;
+	for (string line; getline(fin, line); tiles.push_back(line)) { }
+	const int N = tiles.size(), M = tiles[0].size();
+
+	// Find which directions and locations light is traveling
+	vector<vector<set<char>>> light(N, vector<set<char>>(M, set<char>()));
+	vector<pair<char, pair<int, int>>> heads = { make_pair('>', make_pair(0, 0)) };
+	while (!heads.empty()) {
+		char dir = heads.back().first;
+		int y = heads.back().second.first, x = heads.back().second.second;
+		heads.pop_back();
+
+		if (y >= 0 && x >= 0 && y < N && x < M && !light[y][x].contains(dir)) {
+			light[y][x].insert(dir);
+
+			switch (tiles[y][x]) {
+			case '.':
+				move_forward(dir, y, x);
+				heads.push_back(make_pair(dir, make_pair(y, x)));
+				break;
+			case '/':
+				if (dir == '^') dir = '>';
+				else if (dir == '<') dir = 'v';
+				else if (dir == 'v') dir = '<';
+				else if (dir == '>') dir = '^';
+				move_forward(dir, y, x);
+				heads.push_back(make_pair(dir, make_pair(y, x)));
+				break;
+			case '\\':
+				if (dir == '^') dir = '<';
+				else if (dir == '<') dir = '^';
+				else if (dir == 'v') dir = '>';
+				else if (dir == '>') dir = 'v';
+				move_forward(dir, y, x);
+				heads.push_back(make_pair(dir, make_pair(y, x)));
+				break;
+			case '|':
+				if (dir == '^' || dir == 'v') {
+					move_forward(dir, y, x);
+					heads.push_back(make_pair(dir, make_pair(y, x)));
+				}
+				else {
+					heads.push_back(make_pair('^', make_pair(y - 1, x)));
+					heads.push_back(make_pair('v', make_pair(y + 1, x)));
+				}
+				break;
+			case '-':
+				if (dir == '<' || dir == '>') {
+					move_forward(dir, y, x);
+					heads.push_back(make_pair(dir, make_pair(y, x)));
+				}
+				else {
+					heads.push_back(make_pair('<', make_pair(y, x - 1)));
+					heads.push_back(make_pair('>', make_pair(y, x + 1)));
+				}
+				break;
+			default:
+				return -1;
+			}
+		}
+	}
+
+	// Calculate number of energized tiles
+	int energized = 0;
+	for (int y = 0; y != N; ++y) {
+		for (int x = 0; x != M; ++x) {
+			if (!light[y][x].empty()) {
+				++energized;
+			}
+		}
+	}
+	return energized;
+}
